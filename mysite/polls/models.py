fbfe8e465ac5ffcc7aaf02ec5ca3b20a6fd760ae
shinespark <shinespark64@gmail.com>
2018-03-09 20:15:19 +0900

    part5

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -11,6 +11,10 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
 
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
+
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
@@ -19,6 +23,3 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
-
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
